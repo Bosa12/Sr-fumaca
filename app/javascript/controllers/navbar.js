4c617d1468803f5f2c0c5621d01a56482fc58ba3
catriona beamish <riccibozza@gmail.com>
2024-12-20 13:24:39 +0000

    foto roda de choro

--- app/javascript/controllers/navbar.js
+++ app/javascript/controllers/navbar.js
@@ -10,3 +10,7 @@ menuToggle.addEventListener("click", function () {
     navbarMenu.style.display = "block";
   }
 });
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+  const menu = document.querySelector('#navbarNav');
+  menu.classList.toggle('show');
+});
